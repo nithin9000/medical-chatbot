38a80bb7daf5802ec35dfc67e778b6bb7b3fb4f2
nithin9000 <nithinkrishnalal303@gmail.com>
2025-05-09 19:27:35 +0530

    app

--- backend/core/test_llm.py
+++
@@ -1,9 +0,0 @@
-from llm_model import generate_answer
-
-query = "I have chest pain what to do?"
-prompt = f"""You are a helpfull medical assistant.
-User:{query}
-Answer:"""
-
-response = generate_answer(prompt)
-print("LLM Response:\n",response)
--- backend/database/db_connection.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-
-MONGO_URI = "mongodb://localhost:27017/"
-client = MongoClient(MONGO_URI)
-
-db = client["MedicalDB"]
-hospital_collection = db["Hospitals"]
